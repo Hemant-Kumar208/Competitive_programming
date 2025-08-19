2bd0adeb7b7f9a2dcfedb04eede0c942edc094cc
Hemant-Kumar208 <himanshuchaudhary5048@gmail.com>
2025-08-19 13:40:41 +0530

    Update divideby3_lastno4.py

--- control statments python/divideby3_lastno4.py
+++ control statments python/divideby3_lastno4.py
@@ -2,6 +2,7 @@ num = int(input("Enter a number : "))
 a = num%3
 b = num%10
 if a == 0 and b==4:
-    print("no. is divisible by 3 and last digit is 4")
+    print("no. is divisible by 3 or last digit is 4")
 else:
-    print("either no. is not divisible by 3 or the last digit is not 4")
+
+    print("no. is not divisible by 3 and the last digit is not 4")
