165fed0701253404509962d3ecaf203cb6b1df39
Hemant-Kumar208 <himanshuchaudhary5048@gmail.com>
2025-09-15 09:50:53 +0530

    Update space_pattern.py
    i updated my previous code

--- Star_pattern/space_pattern.py
+++ Star_pattern/space_pattern.py
@@ -1,10 +1,8 @@
 n = int(input("Enter a number: "))
-m = int(input("Enter a number: "))
 for i in range (0,n):
-    for j in range(0,m):
-         if j==1 or j==3:
-              print("*",end=" ")
-         else:
-              print(" ",end=" ")
-    print()
+     print("*",end="")
+     for j in range(n-2):
+          print(" ",end=" ")
+     print("*",end="")
+     print()
     
